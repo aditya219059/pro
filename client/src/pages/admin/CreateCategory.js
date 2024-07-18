9a325e9612c97e45c4ac912ace4e4bf02a45fe18
Aditya <adityat_219059@saitm.org>
2024-07-18 16:29:20 +0530

    nothing

--- client/src/pages/admin/CreateCategory.js
+++ client/src/pages/admin/CreateCategory.js
@@ -1,8 +1,28 @@
-import React from 'react'
-import AdminMenu from '../../components/Layout/AdminMenu'
-import Layout from '../../components/Layout/Layout'
+import React, { useState, useEffect } from "react";
+import AdminMenu from "../../components/Layout/AdminMenu";
+import Layout from "../../components/Layout/Layout";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const CreateCategory = () => {
+  const [categories, setCategories] = useState([]);
+
+  //get all categories
+  const getAllCategory = async () => {
+    const { data } = await axios.get("/api/v1/category/get-category");
+    try {
+      if (data.success) {
+        setCategories(data.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrongin getting category");
+    }
+  };
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
   return (
     <Layout title={"Dashboard Create Category"}>
       <div className="container-fluid m-3 p-3">
@@ -11,12 +31,36 @@ const CreateCategory = () => {
             <AdminMenu />
           </div>
           <div className="col-md-9">
-          <h1>Create Category</h1>
+            <h1>Manage Category</h1>
+            <div className="w-75">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {categories?.map((c) => (
+                    <>
+                      <tr>
+                        <td key={c._id}>{c.name}</td>
+                        <td>
+                          <button className="btn btn-primary" key={c._id}>
+                            Edit
+                          </button>
+                        </td>
+                      </tr>
+                    </>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default CreateCategory
+export default CreateCategory;
