0dc91f77a0f71131fde386aa738f68dee9a270a8
Aditya <adityat_219059@saitm.org>
2024-07-29 11:48:02 +0530

    margin error on top

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -35,7 +35,7 @@ const Header = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <Link to="/" className="navbar-brand" href="#">
-              <FiShoppingCart /> Ecommerce app
+              <FiShoppingCart style={{marginRight: "10px", marginTop: "3px"}} /> Ecommerce app
             </Link>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
