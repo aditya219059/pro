185b36ef4cc62da5a28db84ce8318f66e5acd24b
Aditya <adityat_219059@saitm.org>
2024-08-12 17:10:24 +0530

    user profile 9.17.55

--- client/src/pages/User/Profile.js
+++ client/src/pages/User/Profile.js
@@ -1,8 +1,49 @@
-import React from 'react'
-import UserMenu from '../../components/Layout/UserMenu'
-import Layout from '../../components/Layout/Layout'
+import React, { useEffect, useState } from "react";
+import UserMenu from "../../components/Layout/UserMenu";
+import Layout from "../../components/Layout/Layout";
+import { useAuth } from "../../context/auth";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const Profile = () => {
+  const [auth, setAuth] = useAuth();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    const {email, name, phone, address} = auth?.user;
+    setName(name)
+    setPhone(phone)
+    setAddress(address)
+    setEmail(email)
+  },[auth?.user])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/register`, {
+          name,
+          email,
+          phone,
+          address,
+          password,
+        });
+      if (res && res.data.success) {
+        toast.success(res.data && res.data.message);
+
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
   return (
     <Layout title={"Dashboard - Profile"}>
       <div className="container-fluid m-3 p-3">
@@ -11,12 +52,76 @@ const Profile = () => {
             <UserMenu />
           </div>
           <div className="col-md-9">
-          <h1>Profile</h1>
+            <div className="register">
+              <h2>User Profile</h2>
+              <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="exampleInputname"
+                    placeholder="Enter your name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleInputemail"
+                    placeholder="Enter your email address"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    disabled
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="exampleInputphone"
+                    placeholder="Phone No."
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="exampleInputaddress"
+                    placeholder="Address"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+
+                <button type="submit" className="btn btn-primary">
+                  Update
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
