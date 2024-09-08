7a1284a8f96299a3ffd6e7e3e57b557d50cf323b
aditya1144 <adityanad114@gmail.com>
2024-09-08 16:10:47 +0530

    nothing

--- client/src/pages/admin/CreateProduct.js
+++ client/src/pages/admin/CreateProduct.js
@@ -75,7 +75,6 @@ const CreateProduct = () => {
             <h1>Create Product</h1>
             <div className="m-1 w-75">
               <Select
-                style={{color:"#ffffff", backgroundColor:"#020818"}}
                 placeholder="Select a category"
                 size="large"
                 showSearch
