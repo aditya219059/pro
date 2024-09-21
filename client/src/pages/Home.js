ad8ffb40d4ea6506cbd3a268c619a85309a410cb
aditya1144 <adityanad114@gmail.com>
2024-09-21 14:25:24 +0530

    nothing

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -124,9 +124,9 @@ const Home = () => {
   return (
     <Layout title={"Home - Ecommerce"}>
       {/* <Slider /> */}
-      <div className="word">
-        <h1 className="word-head" style={{height: "100vh"}}>new</h1>
-      </div>
+      {/* <div className="word">
+        <div className="word-head">MOMENTO</div>
+      </div> */}
       <div className="row">
         <div className="col-md-2 mt-3">
           <div className="sticky-top">
